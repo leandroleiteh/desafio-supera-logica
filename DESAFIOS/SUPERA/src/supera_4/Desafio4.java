442991e72c3f5d4f3e31a224ed7245f7a21f5157
leandroleiteh <leandroluz201616@gmail.com>
2023-07-12 16:16:37 -0300

    Feito desafio 4

--- DESAFIOS/SUPERA/src/supera_4/Desafio4.java
+++ DESAFIOS/SUPERA/src/supera_4/Desafio4.java
@@ -1,8 +1,32 @@
 package supera_4;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Desafio4 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        List<String> listMess = new ArrayList<>();
+
+        System.out.println("Digite a quantidade de frases que deseja corrigir: ");
+        int n = sc.nextInt();
+
+        System.out.println("Digite as frases: ");
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String prharse = sc.nextLine();
+            listMess.add(prharse);
+        }
+
+        for (String list : listMess) {
+            StringBuilder part1 = new StringBuilder(list.substring(0, list.length() / 2));
+            StringBuilder part2 = new StringBuilder(list.substring(list.length() / 2, list.length()));
+            part1.reverse();
+            part2.reverse();
+            System.out.println(part1.toString() + part2.toString());
+        }
 
     }
 }
